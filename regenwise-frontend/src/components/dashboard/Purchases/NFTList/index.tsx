8e4e5843031df3f5bf629c887ad9cb1f66473ffd
Harun <harunbcolak@hotmail.com>
2023-06-13 20:35:16 +0300

    2.0.0

--- regenwise-frontend/src/components/dashboard/Purchases/NFTList/index.tsx
+++ regenwise-frontend/src/components/dashboard/Purchases/NFTList/index.tsx
@@ -1,6 +1,5 @@
-import { CardGroup, Card } from 'react-bootstrap';
+import { CardGroup } from 'react-bootstrap';
 import DashboardHeader from '../../shared/DashboardHeader';
-import styles from './index.module.scss';
 import NFTCard from './NFTCard';
 import { NFTItem } from '@src/models/nftItem';
 
@@ -11,25 +10,23 @@ interface Props {
 }
 export default function NFTList({ header, nftItems, executeRelist }: Props) {
   return (
-    <div className={styles.main_container}>
+    <>
       <DashboardHeader header={header} />
-      <div className={styles.content}>
-        <CardGroup className="flex flex-row flex-wrap gap-4">
-          {nftItems.length > 0 ? (
-            nftItems.map((card: NFTItem) => {
-              return (
-                <NFTCard
-                  key={card.description}
-                  card={card}
-                  executeRelist={executeRelist}
-                />
-              );
-            })
-          ) : (
-            <div>There is no item to show under this category.</div>
-          )}
-        </CardGroup>
-      </div>
-    </div>
+      <CardGroup className="flex flex-row justify-center flex-wrap gap-12">
+        {nftItems.length > 0 ? (
+          nftItems.map((card: NFTItem) => {
+            return (
+              <NFTCard
+                key={card.description}
+                card={card}
+                executeRelist={executeRelist}
+              />
+            );
+          })
+        ) : (
+          <div>There is no item to show under this category.</div>
+        )}
+      </CardGroup>
+    </>
   );
 }
