3a9d8e59ed39ad676a5c659a31ad1920e7796394
keenregen <keenregen@gmail.com>
2023-06-24 17:50:19 +0300

    5.3.3

--- regenwise-frontend/src/components/home/NFTMarketPlace/index.tsx
+++ regenwise-frontend/src/components/home/NFTMarketPlace/index.tsx
@@ -51,7 +51,7 @@ export default function NFTMarketPlace() {
                 projects and be supported. After they prove the regen effort(s) that
                 they have been working on, they can be tokenized via the platform and held or traded as
                 NFTs. These NFTs are expected to serve as symbols of support to the crucial
-                contributions to regen goal.
+                contributions to the regen goal.
               </span>
             </p>
           </article>
--- regenwise-frontend/src/components/home/ProjectFeatures/ContentArea/index.tsx
+++ regenwise-frontend/src/components/home/ProjectFeatures/ContentArea/index.tsx
@@ -10,16 +10,16 @@ export default function ContentArea() {
     <div className="mt-5 flex flex-col max-w-[600px] w-full items-center justify-center" id='main-page--content-element'>
       <SectionHeader
         title="Overcoming Degradation via Regeneration"
-        subtitle="For those eager to learn and make a meaningful contribution, this platform provides a diverse range of resources to delve into the concepts and efforts of regeneration."
+        subtitle="For those eager to learn and make a meaningful contribution to the regen goal, this platform provides a diverse range of resources to delve into the concepts and practices of regeneration."
       />
       <p className="text-center"  >
         {`Human actions have caused significant environmental degradation and serious harm to the inhabitants, now it is crucial for us to contribute 
         to the process of regeneration. "Regeneration" in our context is a term used to describe the restoration and responsible management of environmental 
-        systems. Our regen (the short form of the word regeneration) web platform aims to facilitate this contribution by offering various services and resources to users. The 
+        systems. Our regen (the short form of the word regeneration) web platform aims to facilitate the regen efforts by offering various services and resources to users. The 
         platform's two main database collections on regen concepts and regen projects serve as the primary features, connecting it to the world of data analytics and trustworthy web 3 database technologies. 
-        The platform's NFT markets is another essential feature that users may benefit from. The users can earn, buy, and sell valuable
-        NFTs from our platform's collections while spending valuable time to learn and contribute to the regen goal. Additionally, we offer a unique learning 
-        experience through games that educate users about regen concepts and practices in fields like agriculture and energy.`}
+        In addition, the platform's NFT markets are essential features that users may benefit from. The users can earn and trade valuable
+        NFTs from regen collections while spending valuable time to learn and contribute to the regen goal. Additionally, we offer a unique learning 
+        experience through quests that educate users about regen concepts and practices in various fields.`}
       </p>
       <div className="text-center">
         <Button
