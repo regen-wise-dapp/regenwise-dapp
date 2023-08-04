3bdc14ec60fbebb069983ce0d5d10f5f15578e13
keenregen <keenregen@gmail.com>
2023-08-04 18:39:02 +0300

    5.20.0

--- regenwise-frontend/src/components/home/ProjectFeatures/ContentArea/index.tsx
+++ regenwise-frontend/src/components/home/ProjectFeatures/ContentArea/index.tsx
@@ -9,14 +9,14 @@ export default function ContentArea() {
   return (
     <div className="mt-5 flex flex-col max-w-[600px] w-full items-center justify-center" id='main-page--content-element'>
       <SectionHeader
-        title="Overcoming Degradation via Regeneration"
+        title="Overcoming Environmental Degradation via Environmental Regeneration"
         subtitle="For those eager to learn and make a meaningful contribution to the regen goal, this platform provides a diverse range of resources to delve into the concepts and practices of regeneration."
       />
       <p className="text-center"  >
         {`Human actions have caused significant environmental degradation and serious harm to the inhabitants, now it is crucial for us to contribute 
         to the process of regeneration. "Regeneration" in our context is a term used to describe the restoration and responsible utilization and management of environmental 
         systems. Our regen (the short form of the word regeneration) web platform aims to facilitate the regen efforts by offering various services and resources to users. The 
-        platform's two main database collections on regen concepts and regen projects serve as the primary features, connecting it to the world of data analytics and trustworthy web 3 database technologies. 
+        platform's main database collection on regen concepts serves as the primary feature that informs about environmental responsibility and also directs individuals to actively contribute to the regen goal. 
         In addition, the platform's NFT markets are essential features that users may benefit from. The users can earn and trade valuable
         NFTs from regen collections while spending valuable time to learn and contribute to the regen goal. Additionally, we offer a unique learning 
         experience through quests that educate users about regen concepts and practices in various fields.`}
